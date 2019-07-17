9d7c8879fcde779becda81705bf3c4ebcaf3c2f1
Pedro de Mattos <pedrodemattos86@gmail.com>
2019-07-16 21:48:44 -0300

    trabalhando com arrays e for

--- js/principal.js
+++ js/principal.js
@@ -2,35 +2,39 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(i =0; i < pacientes.length; i++){
+    var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdImc = paciente.querySelector(".info-imc");
 
 
 
-pesoEhValido = true;
-alturaEhValida = true;
+    pesoEhValido = true;
+    alturaEhValida = true;
 
-if(peso<=10 || peso>=300){
-    pesoEhValido = false;
-    tdImc.textContent = "inválido";
+    if(peso<=10 || peso>=300){
+        pesoEhValido = false;
+        tdImc.textContent = "inválido";
 
-}
+    }
 
-if(altura<=0 || altura>=3){
-    alturaEhValida = false;
-    tdImc.textContent = "inválido";
-}
+    if(altura<=0 || altura>=3){
+        alturaEhValida = false;
+        tdImc.textContent = "inválido";
+    }
 
-if(alturaEhValida && pesoEhValido){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    if(alturaEhValida && pesoEhValido){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
 
+    }
 }
